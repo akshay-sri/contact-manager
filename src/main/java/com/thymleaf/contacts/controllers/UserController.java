dacb7f5bf6cf6b565dd9fc5b72e1e9b1e60bbe0f
akshay-sri <sriakshay2611999@gmail.com>
2023-12-03 22:56:13 +0530

    sidebar

--- src/main/java/com/thymleaf/contacts/controllers/UserController.java
+++ src/main/java/com/thymleaf/contacts/controllers/UserController.java
@@ -1,13 +1,25 @@
 package com.thymleaf.contacts.controllers;
 
+import com.thymleaf.contacts.entities.User;
+import com.thymleaf.contacts.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Controller
-@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserRepo userRepo;
     @RequestMapping("/index")
-    public String dashboard(){
+    public String dashboard(Model model, Principal principal)
+    {
+        String userName=principal.getName();
+        User user=userRepo.getUserByUserName(userName);
+        model.addAttribute("user",user);
+        model.addAttribute("title", "Dashboard - MyContacts");
         return "user_dashboard";
     }
 }
